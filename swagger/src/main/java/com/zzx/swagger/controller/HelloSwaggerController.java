06d402950b7605ab3544a6cc71f980680249cb3d
zzx <2630613378@qq.com>
2022-08-15 14:21:59 +0800

    master commit01

--- swagger/src/main/java/com/zzx/swagger/controller/HelloSwaggerController.java
+++ swagger/src/main/java/com/zzx/swagger/controller/HelloSwaggerController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloSwaggerController {
 
     @GetMapping("/hello")
+    //测试swagger方法
     public String helloSwagger(){
         return "helloSwagger";
     }
